d720bc4ceab02f6f131b0b8a32c2aa20ee63c14b
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-04 00:38:53 +0600

    data load with dependency and search text emlement

--- src/components/Volunteers/Volunteers.js
+++ src/components/Volunteers/Volunteers.js
@@ -1,22 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useVolunteers from "../../hooks/useVolunteers";
 import Activity from "../Activity/Activity";
 import "./Volunteers.css";
 
 const Volunteers = () => {
-  const [volunteers] = useVolunteers();
+  // const [volunteers] = useVolunteers();
+  const [searchText, setSearchText] = useState("");
   const [searchResult, setSearchResult] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const match = data.filter((data) => data.title.includes(searchText));
+        setSearchResult(match);
+      });
+  }, [searchText]);
+
   const handleSearch = (event) => {
-    const search = event.target.value;
-    const searchResult = volunteers.filter((volunteer) =>
-      volunteer.title.includes(search)
-    );
-    setSearchResult(searchResult);
+    setSearchText(event.target.value);
   };
+  // const handleSearch = (event) => {
+  //   const search = event.target.value;
+  //   const searchResult = volunteers.filter((volunteer) =>
+  //     volunteer.title.includes(search)
+  //   );
+  //   setSearchResult(searchResult);
+  // };
 
   return (
     <div>
-      <h3>Volunteers:{volunteers.length}</h3>
+      <h3>Volunteers:{searchResult.length}</h3>
       <input
         onChange={handleSearch}
         type="text"
