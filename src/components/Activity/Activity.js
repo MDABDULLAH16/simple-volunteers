a99af30fb57e54088f8ee97272e3e0063564d837
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-04 00:06:11 +0600

    search emliment with simple code

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -5,7 +5,7 @@ const Activity = ({ volunteer }) => {
   return (
     <div className="activity">
       <img width="100%" src={volunteer.img} alt="" />
-      <h4 style={{ "margin-top": "0" }}>{volunteer.title}</h4>
+      <h4>{volunteer.title}</h4>
     </div>
   );
 };
--- src/components/Volunteers/Volunteers.js
+++ src/components/Volunteers/Volunteers.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import useVolunteers from "../../hooks/useVolunteers";
 import Activity from "../Activity/Activity";
 import "./Volunteers.css";
 
 const Volunteers = () => {
   const [volunteers] = useVolunteers();
+  const [searchResult, setSearchResult] = useState([]);
+  const handleSearch = (event) => {
+    const search = event.target.value;
+    const searchResult = volunteers.filter((volunteer) =>
+      volunteer.title.includes(search)
+    );
+    setSearchResult(searchResult);
+  };
+
   return (
     <div>
       <h3>Volunteers:{volunteers.length}</h3>
+      <input
+        onChange={handleSearch}
+        type="text"
+        placeholder="search"
+        style={{ margin: "20px" }}
+      />
       <div className="activity-container">
-        {volunteers.map((volunteer) => (
+        {searchResult.map((volunteer) => (
           <Activity key={volunteer._id} volunteer={volunteer}></Activity>
         ))}
       </div>
