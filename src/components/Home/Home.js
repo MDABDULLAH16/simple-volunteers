8f45a5a88de376cd9f59c196624572634c1cf1f0
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-04 16:00:32 +0600

    search recap

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useVolunteers from "../../hooks/useVolunteers";
 import Activity from "../Activity/Activity";
 
 const Home = () => {
-  const [volunteers] = useVolunteers();
+  // const [volunteers] = useVolunteers();
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const match = data.filter((data) => data.title.includes(searchText));
+        setSearchResult(match);
+      });
+  }, [searchText]);
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+  // const handleSearchChange = (event) => {
+  //   const search = event.target.value;
+  //   const match = volunteers.filter((volunteer) =>
+  //     volunteer.title.includes(search)
+  //   );
+  //   setSearchResult(match);
+  // };
+
   return (
     <div>
-      <h2>Volunteers Main Activities: {volunteers.length} </h2>
+      <h2>Volunteers Main Activities: {searchResult.length} </h2>
+      <input
+        onChange={handleSearchChange}
+        type="text"
+        placeholder="Search hare"
+      />
       <div className="activity-container">
-        {volunteers.map((volunteer) => (
+        {searchResult.map((volunteer) => (
           <Activity key={volunteer._id} volunteer={volunteer}></Activity>
         ))}
       </div>
